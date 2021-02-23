bd0c276294e2bec0b1c207b9b7a31e9ed270bbd6
jaydamon <Heron2128#>
2021-02-22 22:23:01 -0500

    Consolidate config, update CORS
    Cleanup config
    
    Remove cors configuration
    
    Code cleanup

--- src/main/java/com/factotum/setzer/config/JpaConfiguration.java
+++ src/main/java/com/factotum/setzer/config/JpaConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
@@ -16,7 +15,7 @@ import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 public class JpaConfiguration {
 
     @Bean
-    @Profile("test")
+    @Profile({"test", "h2"})
     public ConnectionFactoryInitializer initializer(@Qualifier("connectionFactory") ConnectionFactory connectionFactory) {
         ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
         initializer.setConnectionFactory(connectionFactory);
