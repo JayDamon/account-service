1717dcd45ef5fc36f01aeac103204806ec8e2367
= <Jay.n.damon@gmail.com>
2021-08-27 09:16:34 -0400

    fix(db): Fix order of db upgrade statements

--- src/main/resources/db/migration/V2_3__add_non_null_constraint.sql
+++ src/main/resources/db/migration/V2_3__add_non_null_constraint.sql
@@ -1,5 +1,4 @@
+UPDATE account SET current_balance = 0 WHERE current_balance IS NULL;
+
 ALTER TABLE account MODIFY current_balance decimal NOT NULL;
 
-UPDATE account
-SET current_balance = 0
-WHERE current_balance IS NULL;
