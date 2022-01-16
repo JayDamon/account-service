08cae3b381dbdce3d2602c8419f462c7e14c9d0a
JayDamon <Jay.n.damon@gamil.com>
2022-01-15 21:47:07 -0500

    fix(build): Update docker java version

--- src/main/java/com/factotum/setzer/repository/AccountRepositoryCustom.java
+++ src/main/java/com/factotum/setzer/repository/AccountRepositoryCustom.java
@@ -2,7 +2,6 @@ package com.factotum.setzer.repository;
 
 import com.factotum.setzer.dto.AccountDto;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import reactor.core.publisher.Flux;
 
 public interface AccountRepositoryCustom {
