1f3affc6971bc5ecd5e934242bc9abc7de1835e5
jaydamon <jay.n.damon@gmail.com>
2022-02-06 09:02:25 -0500

    refactor: Update local/test data to match new keycloak user

--- src/main/resources/test_data/V2_1__add_tenant_id_in_tables.sql
+++ src/main/resources/test_data/V2_1__add_tenant_id_in_tables.sql
@@ -1,4 +1,4 @@
-update account SET tenant_id = '684996db-6cf8-4976-8336-6e664386dcda';
+update account SET tenant_id = '5809b48e-b705-4b3e-be9f-16ce0380cb45';
 
 INSERT INTO account (name, account_type_id, starting_balance, current_balance, is_primary_account, is_in_cash_flow, tenant_id)
 VALUES ('Tenant 2 checking', 1, 500.01, 8000.56, 1, 1, '7c21bdec-219e-4492-a2fd-e6a2ec5fb8fa');
